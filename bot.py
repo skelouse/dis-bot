a668ee25e57d04747b470f096f13f85a75371f0d
skelouse <sam@skelouse.com>
2020-07-11 14:37:43 -0500

    Added role requirement

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import ast
 import sys
 import discord
+from discord.utils import get
 from dotenv import load_dotenv
 from io import StringIO
 
@@ -22,17 +23,27 @@ class CustomClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        
-        if message.content.startswith("```") and message.content.endswith("```"):
-            old_stdout = sys.stdout
-            redirected_output = sys.stdout = StringIO()
-            exec(message.content[3:-3])
-            sys.stdout = old_stdout
 
-            response = redirected_output.getvalue()
-            if not response:
-                response = 'None'
-            await message.channel.send(response)
+        if message.content == '/help':
+            await message.channel.send("'''(code here)'''  <-  Replace the ' with `")
+
+        if message.content.startswith("```") and message.content.endswith("```"):
+            botuser = False
+            for i in message.author.roles:
+                if i.name == "Bot User":
+                    botuser = True
+            if botuser:
+                old_stdout = sys.stdout
+                redirected_output = sys.stdout = StringIO()
+                exec(message.content[3:-3])
+                sys.stdout = old_stdout
+
+                response = redirected_output.getvalue()
+                if not response:
+                    response = 'None'
+                await message.channel.send(response)
+            else:
+                await message.channel.send("You are not allowed to use this bot, please request permission")
 
 client = CustomClient()
 client.run(TOKEN)
