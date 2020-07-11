e7209dca0437a296b98ef89f22f6a75bdc3ccc9e
skelouse <sam@skelouse.com>
2020-07-10 20:12:55 -0500

    done

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 ## bot.py
 import os
-
+import ast
+import sys
 import discord
 from dotenv import load_dotenv
+from io import StringIO
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -22,8 +24,15 @@ class CustomClient(discord.Client):
         if message.author == self.user:
             return
         
-        if message.content == '99':
-            response = 'ayye'
+        if message.content.startswith("```") and message.content.endswith("```"):
+            old_stdout = sys.stdout
+            redirected_output = sys.stdout = StringIO()
+            exec(message.content[3:-3])
+            sys.stdout = old_stdout
+
+            response = redirected_output.getvalue()
+            if not response:
+                response = 'None'
             await message.channel.send(response)
 
 client = CustomClient()
